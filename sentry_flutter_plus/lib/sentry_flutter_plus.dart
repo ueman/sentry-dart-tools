72ee0329b4206828a2cc176f51a28f94ca3f2663
Jonas Uekötter <dev@uekoetter.dev>
2022-04-30 20:05:33 +0200

    moar

--- sentry_flutter_plus/lib/sentry_flutter_plus.dart
+++ sentry_flutter_plus/lib/sentry_flutter_plus.dart
@@ -1,7 +1,4 @@
 library sentry_flutter_plus;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/extension.dart';
+export 'src/method_channel/sentry_binary_messenger.dart';
--- sentry_flutter_plus/lib/src/extension.dart
+++ sentry_flutter_plus/lib/src/extension.dart
@@ -1,4 +1,9 @@
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry_flutter_plus/src/event_processor/flutter_event_processor.dart';
+import 'package:sentry_flutter_plus/src/event_processor/linux_event_processor.dart';
+import 'package:sentry_flutter_plus/src/event_processor/windows_event_processor.dart';
+import 'package:sentry_flutter_plus/src/integrations/exclude_integrations.dart';
+import 'package:sentry_flutter_plus/src/integrations/on_error_integration.dart';
 import 'package:sentry_plus/sentry_plus.dart';
 
 extension SentryFlutterPlus on SentryFlutterOptions {
@@ -13,5 +18,12 @@ extension SentryFlutterPlus on SentryFlutterOptions {
       addHttpTracing: addHttpTracing,
       addUnhandledEventProcessor: addUnhandledEventProcessor,
     );
+
+    addEventProcessor(FlutterEventProcessor());
+    addEventProcessor(LinuxEventProcessor());
+    addEventProcessor(WindowsEventProcessor());
+
+    addIntegration(ExcludeIntegration());
+    addIntegration(OnErrorIntegration());
   }
 }
