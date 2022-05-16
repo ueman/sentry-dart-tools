1624290394016bb15bc5c02ff54428ef30b11bd0
Jonas Uekötter <ueman@users.noreply.github.com>
2022-05-16 20:45:10 +0200

    Update file_extension.dart

--- sentry_plus/lib/src/file/file_extension.dart
+++ sentry_plus/lib/src/file/file_extension.dart
@@ -2,6 +2,8 @@ import 'package:sentry/sentry.dart';
 import '_io.dart' if (dart.library.html) '_no_op.dart' as file_tracer;
 
 extension SentryFileExtension on SentryOptions {
+  // Must be called outside the RunZoneGuardedIntegration.
+  // Otherwise the FileOverrides aren't attached to the root zone.
   void addFileTracing() {
     file_tracer.addFileTracing(this);
   }
