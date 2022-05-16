7adf5ede381c04328289419d386cb027e3d9d9a5
Jonas Uekötter <ueman@users.noreply.github.com>
2022-05-16 20:45:52 +0200

    Update http_extension.dart

--- sentry_plus/lib/src/http/http_extension.dart
+++ sentry_plus/lib/src/http/http_extension.dart
@@ -2,5 +2,7 @@ import 'package:sentry/sentry.dart';
 import '_io.dart' if (dart.library.html) '_no_op.dart' as http_tracer;
 
 extension SentryHttpExtension on SentryOptions {
+  // Must be called outside the RunZoneGuardedIntegration.
+  // Otherwise the HttpOverrides aren't attached to the root zone.
   void addHttpTracing() => http_tracer.addHttpTracing(this);
 }
