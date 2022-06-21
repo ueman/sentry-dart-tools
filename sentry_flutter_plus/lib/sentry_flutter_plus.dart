86e8f8222a66baafe4b8bfec7baa586a2308ed22
Jonas Uekötter <dev@uekoetter.dev>
2022-06-21 18:22:18 +0200

    tree walker attachment

--- sentry_flutter_plus/lib/sentry_flutter_plus.dart
+++ sentry_flutter_plus/lib/sentry_flutter_plus.dart
@@ -6,3 +6,4 @@ export 'src/widget_tree_attachment.dart';
 export 'src/integrations/platform_menu_integration.dart'
     show PlatformMenuIntegration;
 export 'src/integrations/binding_integration.dart';
+export 'src/integrations/tree_walker_integration.dart';
---
+++ sentry_flutter_plus/lib/src/integrations/tree_walker_integration.dart
@@ -0,0 +1,109 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+class TreeWalkerIntegration extends Integration<SentryFlutterOptions> {
+  @override
+  FutureOr<void> call(Hub hub, SentryFlutterOptions options) {
+    hub.configureScope((scope) {
+      scope.addAttachment(TreeAttachment());
+    });
+    options.sdk.addIntegration('TreeWalkerIntegration');
+  }
+}
+
+class TreeWalker {
+  final Element? rootElement;
+
+  TreeWalker(this.rootElement);
+
+  Node? compute() {
+    final root = rootElement;
+    if (root == null) {
+      return null;
+    }
+    final rootNode = Node(root.widgetTypeName, root.widget.key);
+    root.visitChildElements(_visitor(rootNode));
+    return rootNode;
+  }
+
+  ValueChanged<Element> _visitor(Node parentNode) {
+    return (Element element) {
+      final node = Node(element.widgetTypeName, element.widget.key);
+      parentNode.children.add(node);
+      element.visitChildElements(_visitor(node));
+    };
+  }
+}
+
+class Node {
+  Node(this.name, this.key);
+
+  String name;
+  Key? key;
+  List<Node> children = [];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      if (key != null) 'key': key.toString(),
+      if (children.isNotEmpty)
+        'children': children.map((e) => e.toMap()).toList(growable: false),
+    };
+  }
+
+  String toJsonString() => jsonEncode(toMap());
+
+  @override
+  String toString() {
+    //return jsonEncode(toMap());
+    StringBuffer buffer = StringBuffer();
+    _printAsTree(buffer, '', false);
+    return buffer.toString();
+  }
+
+  void _printAsTree(StringBuffer buffer, String indent, bool last) {
+    buffer.write(indent);
+    if (last) {
+      buffer.write("└╴");
+      indent += "  ";
+    } else {
+      buffer.write("├╴");
+      indent += "│ ";
+    }
+    buffer.writeln(name);
+    if (key != null) {
+      buffer.write(' (Key: $key)');
+    }
+
+    for (int i = 0; i < children.length; i++) {
+      children[i]._printAsTree(buffer, indent, i == children.length - 1);
+    }
+  }
+}
+
+extension on Element {
+  String get widgetTypeName => widget.runtimeType.toString();
+}
+
+class TreeAttachment extends SentryAttachment {
+  TreeAttachment()
+      : super.fromLoader(
+          loader: _widgetTree,
+          filename: 'widget_tree.txt',
+          contentType: 'text/plain',
+        );
+}
+
+FutureOr<Uint8List> _widgetTree() {
+  final tree = TreeWalker(WidgetsBinding.instance.renderViewElement)
+      .compute()
+      ?.toString();
+  if (tree == null) {
+    return Uint8List.fromList([]);
+  }
+  return Uint8List.fromList(utf8.encode(tree));
+}
