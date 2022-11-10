39c39b0e98c2b791f536f1ad04e3d7f4e777e6f8
Jonas Uekötter <dev@uekoetter.dev>
2022-11-10 19:28:47 +0100

    Improve binding. Close #4

--- sentry_flutter_plus/lib/src/integrations/binding_integration.dart
+++ sentry_flutter_plus/lib/src/integrations/binding_integration.dart
@@ -8,19 +8,10 @@ import 'package:sentry_flutter_plus/src/widgets/sentry_widget.dart';
 
 import '../method_channel/sentry_binary_messenger.dart';
 
-/// Must be used instead of [WidgetsFlutterBindingIntegration].
-/// If [WidgetsFlutterBindingIntegration] is used, this won't have
-/// any effect.
-/// Therefore, when using this integration, [WidgetsFlutterBindingIntegration]
-/// must be removed beforehand.
-///
 /// Must be used instead of [WidgetsFlutterBinding] and
 /// [WidgetsFlutterBinding.ensureInitialized].
 /// If [WidgetsFlutterBinding] is initialized before, this
 /// does not do anything.
-// ignore: todo
-// TODO: Make a mixin out of this so this could be used with other peoples
-// custom bindings
 class WidgetsSentryBinding extends BindingBase
     with
         GestureBinding,
@@ -29,15 +20,28 @@ class WidgetsSentryBinding extends BindingBase
         PaintingBinding,
         SemanticsBinding,
         RendererBinding,
-        WidgetsBinding {
-  static bool _initialized = false;
+        WidgetsBinding,
+        SentryBinding {
+  static WidgetsSentryBinding get instance =>
+      BindingBase.checkInstance(_instance);
+  static WidgetsSentryBinding? _instance;
 
-  static WidgetsBinding ensureInitialized() {
-    if (!_initialized) WidgetsSentryBinding();
-    _initialized = true;
-    return WidgetsBinding.instance;
+  @override
+  void initInstances() {
+    super.initInstances();
+    _instance = this;
   }
 
+  static WidgetsSentryBinding ensureInitialized() {
+    _assertNoBindingIsInitialized();
+    if (WidgetsSentryBinding._instance == null) {
+      WidgetsSentryBinding();
+    }
+    return WidgetsSentryBinding.instance;
+  }
+}
+
+mixin SentryBinding on WidgetsBinding {
   @override
   void attachRootWidget(Widget rootWidget) {
     super.attachRootWidget(SentryWidget(child: rootWidget));
@@ -50,4 +54,25 @@ class WidgetsSentryBinding extends BindingBase
       binaryMessenger: super.createBinaryMessenger(),
     );
   }
+}
+
+/// Before initializing [WidgetsSentryBinding], you need to make sure
+/// that no other [BindingBase], like [WidgetsFlutterBinding], was initialized
+/// before.
+///
+/// In release mode ([kReleaseMode]), there's no way to check whether a binding
+/// was already initialized.
+/// Therefore, in release mode this method is a no-op.
+///
+/// In debug mode ([kDebugMode]), this method returns normally if no binding was
+/// initialized.
+/// It throws an [AssertionError] if a binding was previously initialized.
+void _assertNoBindingIsInitialized() {
+  assert(
+    BindingBase.debugBindingType() == null,
+    "Make sure you don't use any other Bindings than `WidgetsSentryBinding`. "
+    'If you want to use other Bindings either opt out of using the '
+    '`WidgetsSentryBinding` or create your own custom Binding and mixin '
+    '`SentryBinding`.',
+  );
 }
