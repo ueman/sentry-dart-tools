87d6ad099df9489e05688ba2ac53b205838731e3
Jonas Uekötter <ueman@users.noreply.github.com>
2023-01-07 21:30:49 +0100

    Update sentry_plus.dart

--- sentry_plus/lib/sentry_plus.dart
+++ sentry_plus/lib/sentry_plus.dart
@@ -1,6 +1,5 @@
 library sentry_plus;
 
-export 'src/file/file_extension.dart';
 export 'src/http/http_extension.dart';
 export 'src/convert/extension.dart';
 export 'src/event_processor/unhandled_event_processor.dart';
