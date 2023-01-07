7364dfdeb7bbf1efda959287ce72402a24d582a5
Jonas Uekötter <ueman@users.noreply.github.com>
2023-01-07 21:29:46 +0100

    Delete sentry_plus/lib/src/file directory

--- sentry_plus/lib/src/file/_io.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:sentry/sentry.dart';
-import 'package:sentry_plus/src/file/sentry_io_overrides.dart';
-
-void addFileTracing(SentryOptions options) {
-  IOOverrides.global = SentryIoOverrides(HubAdapter(), options);
-}
--- sentry_plus/lib/src/file/_no_op.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sentry/sentry.dart';
-
-void addFileTracing(SentryOptions options) {
-  options.logger(
-    SentryLevel.info,
-    'This platform does not support file tracing. Doing a no op.',
-  );
-}
