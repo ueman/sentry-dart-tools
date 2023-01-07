eee23eddb1052c553827d1ced6e80b31b7a90b4c
Jonas Uekötter <ueman@users.noreply.github.com>
2023-01-07 21:30:26 +0100

    Update add_all_extension.dart

--- sentry_plus/lib/src/add_all_extension.dart
+++ sentry_plus/lib/src/add_all_extension.dart
@@ -1,20 +1,15 @@
 import 'package:sentry/sentry.dart';
 import 'package:sentry_plus/src/event_processor/unhandled_event_processor.dart';
-import 'file/file_extension.dart';
 import 'http/http_extension.dart';
 import 'integration/in_app_integration.dart';
 
 extension AddAllExtension on SentryOptions {
   /// Add all automatic integrations
   void addSentryPlus({
-    bool addFileTracing = true,
     bool addHttpTracing = true,
     bool addUnhandledEventProcessor = true,
     bool inAppIntegration = true,
   }) {
-    if (addFileTracing) {
-      this.addFileTracing();
-    }
     if (addHttpTracing) {
       this.addHttpTracing();
     }
