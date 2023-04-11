7bb3f3758a8aeb457af5a715b636dc0fba68638c
Jonas Uekötter <ueman@users.noreply.github.com>
2023-04-11 13:27:52 +0200

    Update sentry_dart_frog

--- sentry_dart_frog/CHANGELOG.md
+++ sentry_dart_frog/CHANGELOG.md
@@ -1,3 +1,10 @@
+## 0.1.0
+
+- Updated Sentry
+- Updates dart_frog
+- Raised min Dart version to 2.19
+- Added distributed tracing support
+
 ## 0.0.1
 
 - Initial version.
--- sentry_dart_frog/lib/src/tracing_middleware.dart
+++ sentry_dart_frog/lib/src/tracing_middleware.dart
@@ -8,12 +8,42 @@ Handler sentryPerformanceMiddleware(Handler innerHandler) {
     return innerHandler;
   }
   return (context) async {
+    // see https://develop.sentry.dev/sdk/performance/#header-sentry-trace
+
+    final urlDetails =
+        // ignore: invalid_use_of_internal_member
+        HttpSanitizer.sanitizeUrl(context.request.url.toString());
+
+    var description = context.request.method.toString();
+
+    if (urlDetails != null) {
+      description += ' ${urlDetails.urlOrFallback}';
+    }
+
+    final traceHeaderValue = context.request.headers['sentry-trace'];
+    final baggageHeaderValue = context.request.headers['baggage'];
+
+    SentryTransactionContext? trxContext;
+
+    if (traceHeaderValue != null) {
+      trxContext = SentryTransactionContext.fromSentryTrace(
+        description,
+        'http.client',
+        SentryTraceHeader.fromTraceHeader(traceHeaderValue),
+        baggage: baggageHeaderValue != null
+            ? SentryBaggage.fromHeader(baggageHeaderValue)
+            : null,
+      );
+    }
+
     Response response;
-    final trx = Sentry.startTransaction(
-      context.request.uri.toString(),
-      'http',
-      bindToScope: true,
-    );
+    final trx = trxContext != null
+        ? Sentry.startTransactionWithContext(trxContext, bindToScope: true)
+        : Sentry.startTransaction(
+            description,
+            'http.client',
+            bindToScope: true,
+          );
     try {
       response = await innerHandler(context);
       trx.status = SpanStatus.fromHttpStatusCode(response.statusCode);
